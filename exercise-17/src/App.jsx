c2ac6f3c1d2d7d6c10300bf595b8768b2df6c0a4
Devabdalla25 <devabdulla25@gmail.com>
2025-09-20 07:03:56 +0300

    i was uploaded exercise 17 in react exercises

---
+++ exercise-17/src/App.jsx
@@ -0,0 +1,10 @@
+import ContactForm from "./contactForm";
+import useForm from "./useForm";const App = () => {
+  return (
+    <div>
+      <ContactForm />
+    </div>
+  )
+}
+
+export default App;
